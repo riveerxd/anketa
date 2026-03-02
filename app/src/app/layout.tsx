bf7a9cc5a146a3609b520847d96b29cc1093eae5
lukas.hrehor <lukas.hrehor@gmail.com>
2026-03-02 11:33:02 +0100

    Add Google Analytics

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -13,6 +14,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="cs">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-3ZM3QFSFNY"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-3ZM3QFSFNY');
+          `}
+        </Script>
+      </head>
       <body className="antialiased">{children}</body>
     </html>
   )
